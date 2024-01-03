e6402bf3d735b5c190b10075e46743507d750dd3
AlfredoVR <alfredo2.villarroya@gmail.com>
2024-01-03 18:18:51 +0000

    New Class Main

--- src/software/ulpgc/kata2/Main.java
+++ src/software/ulpgc/kata2/Main.java
@@ -1,4 +1,18 @@
 package software.ulpgc.kata2;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        CSVCarLoader carLoader = new CSVCarLoader(new File("CarDatabase.csv"));
+        List<Car> cars = carLoader.load();
+        Map<Integer, Integer> horsepowerOrganized = new HorsepowerCarOrganizer().organize(cars);
+        for (Integer key :
+                horsepowerOrganized.keySet()) {
+            System.out.println(key + "-" + (key+49) + " -> " + horsepowerOrganized.get(key));
+
+        }
+    }
 }
